44217df529da7f25dd029c27c945b4cdacfce22b
Mengzhen Li <90226269+Meng-zhen-Li@users.noreply.github.com>
2024-01-20 13:20:42 -0500

    Add files via upload

--- input_data.py
+++ input_data.py
@@ -12,30 +12,26 @@ def load_data(datastr):
     data = loadmat('data/' + datastr + '.mat')
     edgelist = data['edgelist']
     num_u = data['num_u']
-    testing_nodes = data['testing_nodes'].flatten()
     adj = sp.csr_matrix((np.ones(len(edgelist)), (edgelist[:, 0], edgelist[:, 1])), shape=(
         np.max(edgelist) + 1, np.max(edgelist) + 1))
     adj = adj + adj.transpose()
     adj.data = np.ones(len(adj.data))
     idx = (edgelist[:, 0] < num_u) & (edgelist[:, 1] >= num_u)
     edgelist_bipartite = edgelist[idx[0]]
-    idx = ((edgelist[:, 0] < num_u) & (edgelist[:, 1] < num_u)) | (
-        (edgelist[:, 0] >= num_u) & (edgelist[:, 1] >= num_u))
-    edgelist_no_bipartite = edgelist[idx[0]]
+    testing_node = int(FLAGS.testing_node)
+    if testing_node <= np.max(edgelist_bipartite[:, 0]):
+        idx = ~((edgelist[:, 0] == testing_node) & (edgelist[:, 1] >= num_u))
+    else:
+        idx = ~((edgelist[:, 0] < num_u) & (edgelist[:, 1] == testing_node))
+    edgelist_train = edgelist[idx[0]]
 
-    adj_no_bipartite = sp.csr_matrix((np.ones(len(edgelist_no_bipartite)), (edgelist_no_bipartite[:, 0], edgelist_no_bipartite[:, 1])), shape=(np.max(edgelist) + 1, np.max(edgelist) + 1))
-    adj_no_bipartite = adj_no_bipartite + adj_no_bipartite.transpose()
-    adj_no_bipartite.data = np.ones(len(adj_no_bipartite.data))
+    adj_train = sp.csr_matrix((np.ones(len(edgelist_train)), (edgelist_train[:, 0], edgelist_train[:, 1])), shape=(np.max(edgelist) + 1, np.max(edgelist) + 1))
+    adj_train = adj_train + adj_train.transpose()
+    adj_train.data = np.ones(len(adj_train.data))
 
     if 'features' in data.keys():
         features = data['features']
     else:
         features = sp.identity(adj.shape[0])
 
-    return adj, int(num_u), features, adj_no_bipartite, edgelist_bipartite, testing_nodes
-
-
-def perturb(edgelist, perturb_ratio):
-    idx = (np.random.rand(len(edgelist)) > perturb_ratio)
-    new_edgelist = edgelist[idx]
-    return new_edgelist
+    return adj, int(num_u), features, adj_train, edgelist_bipartite
